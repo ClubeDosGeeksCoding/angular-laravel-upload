62fb898b3e6009a88522bbeb860bc5005e61459b
Jayr Alencar <jayralencarpereira@gmail.com>
2016-06-23 15:59:39 -0300

    Banco de dados

---
+++ database/migrations/2016_06_23_184307_table_imagens.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class TableImagens extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('imagens', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('arquivo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('imagens');
+    }
+}
